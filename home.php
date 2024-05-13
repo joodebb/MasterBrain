eb2f11bf23a842e05d400e499924ca5c6c77db6a
joodebb <josephdebrah92@gmail.com>
2024-05-14 00:10:30 +0100

    Homepage

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>MasterBrain</title>
+    <section class="navbar">
+         <div  class="logo"><img src=> alt="MasterBrain"></div>
+         <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About MasterBrain</a></li>
+            <li><a href="#">Resources</a></li>
+            <li><a href="#">Sign Up</a></li>
+</ul>
+
+</div>
+</section>
+</head>
+<body>
+    
+</body>
+</html>
